434a1a21ea1d889380bfc4a6180bbbd10c8fbf8e
k1llerk4se <mike.kazmer61@gmail.com>
2014-12-07 23:14:51 -0500

    Last Commit?
    Maybe the last commit before a rewrite

--- src/MainBot.java
+++ src/MainBot.java
@@ -1,38 +1,16 @@
 import org.jibble.pircbot.PircBot;
 
 public class MainBot extends PircBot {
-
+	String game;
+	String owner = "k1llerk4se";
     public MainBot() {
       this.setName("k1llerbot");
     }
-    /*public void testSql(String sender){
-        try {
-            Connection myconn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/admins", "user", "p");
-            Statement mystmnt = myconn.createStatement();
-            String SQL = "insert into admins " + " (username, email)" + " values ( , 'k1llerk4se@live.com')";
-            mystmnt.executeUpdate(SQL);
-            //Statement allUser = myconn.createStatement();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }*/
-    /*public String isListed(String sender){
-        String allSenders = null;
-        try {
-            Connection myconn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/admins", "k1llerk4se", "Kazm9495893");
-            Statement allUser = myconn.createStatement();
-            ResultSet allUserRest = allUser.executeQuery("select user from users where user ='" + sender + "'");
-            allSenders = allUserRest.toString();
-            if (allSenders != sender){
-                CallableStatement userCall = myconn.prepareCall("{call insert_user(?)}");
-                userCall.setString(1, allSenders);
-            }
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-        return allSenders;
-    }*/
-
+	public void setGame(String thing){
+		if(thing != null){
+			game = thing;
+		}
+	}
     public void onMessage(String channel, String sender,
                           String login, String hostname, String message) {
         if(message.contains("adblock")) {
@@ -54,16 +32,54 @@ public class MainBot extends PircBot {
         if (message.equalsIgnoreCase("!badbot")){
             sendMessage(channel, "/me hides in a corner");
         }
-        if(message.equalsIgnoreCase("!crash")){
+        if(message.equalsIgnoreCase("!bttv")){
+            sendMessage(channel, "If you don't have it already, download http://nightdev.com/betterttv/ . It's an awesome add-on for your browser which greatly improves your twitch experience.");
+        }
+	    if(message.equalsIgnoreCase("!chatrules")){
+		    sendMessage(channel, "Please keep the chat polite ~ Do NOT ask for mod! ~ Respect the streamer, moderators and other viewers or you will be banned ~ Do NOT talk about age | politics | X-rated materials ~ Do NOT self - advertise this will result in a BAN..... If the rules are not followed or are blatantly trolled and ignored you will get insta-banned.");
+	    }
+        if(message.equalsIgnoreCase("!commands")){
+            sendMessage(channel, "The list of commands for this bot can be found here: https://github.com/k1llerk4se/K1llerBot/wiki/Commands");
+        }
+	    if(message.equalsIgnoreCase("!cookies")){
+		    sendMessage(channel, "Get your own damn cookies man! |∴◉ ϖ ◉∴|");
+	    }
+	    if(message.equalsIgnoreCase("!crash")){
             sendMessage(channel, "Oops it looks like Mikel broke his game AGAIN, get out the popcorn");
         }
-        /*if (message.equalsIgnoreCase("!mod")) {
-            if (sender == "k1llerk4se") {
-                String name;
-                name = message.split(" ")[1];
-                testSql(name);
-            }
-        }*/
+	    if(message.equalsIgnoreCase("!cyrpto")){
+		    sendMessage(channel, "Blockchain: 1Do84zVeeyENr7zQYyMmf3H7PtJ8EoszjW. Coinbase: 53801ee08a7564e74d000066. Doge: 82d1fd4b-84a7-fd03-5746-aad41350e7e0. DogeTip: D6AXcpFNBQoXyG1BpiuJMPoRbr6DyXfzwR.");
+	    }
+        if(message.equalsIgnoreCase("!dnt")){
+            sendMessage(channel, "/me passes the piggy bank https://imraising.tv/u/k1llerk4se/");
+        }
+	    if(message.equalsIgnoreCase("!doc")){
+		    sendMessage(channel, "Go to DOCs' stream: http://www.twitch.tv/calsafuric");
+	    }
+	    if(message.equalsIgnoreCase("!game")){
+		    if(message.split(" ") != null){
+			    String name = message.split(" ")[0];
+			    setGame(name);
+		    }else{
+			    if(game != null){
+					sendMessage(channel, "The current game is:" + game);
+			    }else{
+				    sendMessage(channel, "No game is currently set");
+			    }
+
+		    }
+	    }
+        if (message.equalsIgnoreCase("!GG")){
+            sendMessage(channel, "http://www.myinstants.com/instant/gg/");
+        }
+        if(message.equalsIgnoreCase("!k1llerbot")){
+            sendMessage(channel, "/me is special");
+            sendMessage(channel, "I am custom written chat bot written by k1llerk4se for his channel.");
+        }
+        if(message.equalsIgnoreCase("!nightbot")){
+            sendMessage(channel, "/me Fucks Nightbot");
+	        sendMessage(channel, "I'm K1llerbot");
+        }
         if (message.equalsIgnoreCase("!pif")){
             sendMessage(channel, "Every 50 dollars of donations, I will give away a game on steam. #playitforward.");
         }
@@ -71,29 +87,34 @@ public class MainBot extends PircBot {
             sendMessage(channel, "༼ つ◕_◕༽つ Death or Riot ༼ つ◕_◕༽つ");
         }
         if (message.equalsIgnoreCase("!riot1")) {
-            String riot = message.split(" ")[1];
+            String riot = message.split(" ")[2];
             sendMessage(channel, "༼ つ◕_◕༽つ" + riot + "or Riot ༼ つ◕_◕༽つ");
         }
+	    if(message.equalsIgnoreCase("!sadface")){
+		    sendMessage(channel, "http://i.imgur.com/2qVC8lf.gif?1");
+	    }
         if (message.equalsIgnoreCase("!twitter")) {
             sendMessage(channel, "Follow me on twitter https://twitter.com/k1llerk4se");
+        }
+        if (message.contains("GG")){
+            sendMessage(channel, "http://www.myinstants.com/instant/gg/");
         }
         if (message.equalsIgnoreCase("hey")|| message.equalsIgnoreCase("hi") || message.equalsIgnoreCase("hello")){
             sendMessage(channel, "o/ Heyo \\o. " + sender + " how are you tonight?");
         }
-        if (message.contains("***")) {
+        if (message.contains("***") || message.contains("dick") || message.contains("Dick") || message.contains("DIck") || message.contains("gay")) {
             sendMessage(channel, ".timeout " + sender);
             sendMessage(channel, ".unban " + sender);
             sendMessage(channel, "HEY! " + sender + " Just because I curse that dosen't mean you should so stop it.");
         }
+	    if(message.contains("http:") || message.contains("www.") || message.contains(".com") || message.contains(".me") || message.contains(".edu") || message.contains(".tv") || message.contains(".net") || message.contains(".org") || message.contains("https:")){
+		    if(sender != owner) {
+			    sendMessage(channel, ".timeout " + sender);
+			    sendMessage(channel, ".unban " + sender);
+			    sendMessage(channel, "HEY! " + sender + " NO LINKS IN THE COMS! THEY WILL BE DELETED! LINKS ARE ONLY ALLOWED IN THE COMS WITH MIKEL'S APPROVAL");
+		    }else{
+
+		    }
+		}
     }
-    /*public void onJoin(String channel, String sender, String login, String hostname) {
-        String sqlUser;
-        sqlUser = isListed(sender);
-        if (sqlUser != sender) {
-            sendMessage(channel, "Welcome to the crew " + sender);
-        }
-        else{
-            sendMessage(channel, "Welcome back " + sender + " good to see you again");
-        }
-    }*/
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -5,7 +5,7 @@ public static void main(String[] args){
 	int ircPort = 6667;
 try{
 bot.setVerbose(true);
-bot.connect("irc.twitch.tv", ircPort, "oauth");
+bot.connect("irc.twitch.tv", ircPort, "oauth:sgmrecmyrofj7xgpbypbr6da3jkzdq");
 bot.joinChannel("#k1llerk4se");
 }
 catch (Exception e){e.printStackTrace();}
